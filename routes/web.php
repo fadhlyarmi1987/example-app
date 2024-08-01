bfee3c84217c85e7a18da6ab9777e494bab69ade
AluffAl <aluf031@gmail.com>
2024-08-01 13:26:14 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -77,5 +77,3 @@ Route::middleware(['user_type'])->group(function () {
     Route::get('/karyawan', [KaryawanController::class, 'karyawan'])->name('karyawan');
     // Tambahkan semua rute lain yang ingin dilindungi disini
 });
-
-//cobaa saammm
