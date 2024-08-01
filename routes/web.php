650831583d66390a61de8c64e70c8cd4ec17762b
AluffAl <aluf031@gmail.com>
2024-08-01 10:09:41 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,28 @@ Route::get('/', function () {
 // Route method GET
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+
 Route::get('/magang', [AuthController::class, 'magang'])->name('magang');
+
 Route::get('/absen', [AuthController::class, 'absen'])->name('absen');
+
 Route::get('/notif', [AuthController::class, 'notif'])->name('notif');
+
 Route::get('/tugas', [AuthController::class, 'tugas'])->name('tugas');
 //Route::get('/getAttendance', [AuthController::class, 'getAttendance'])->name('getAttendance');
 // Di dalam routes/web.php atau routes/api.php
 Route::get('/getAttendance', [AuthController::class, 'getAttendance'])->name('getAttendance');
 
 Route::get('/beranda', [BerandaController::class, 'beranda']);
+
 Route::get('/penjumlahan', [BerandaController::class, 'penjumlahan']);
+
 Route::get('/tugas', [FileController::class, 'index']);
+
 Route::get('/unduh/{id}', [FileController::class, 'download']);
+
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
 
 Route::post('/register', [RegisterController::class, 'register'])->name('register.submit'); // Menambahkan route post untuk register
