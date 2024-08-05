73dc327267d88043921518bcc6309284cd1be142
AluffAl <aluf031@gmail.com>
2024-08-05 13:56:40 +0700

    update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar - CV NATUSI</title>
-    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <link rel="stylesheet" href="{{ asset('CSS/register.css') }}">
 </head>
 <body>
     <header class="header-bar">
