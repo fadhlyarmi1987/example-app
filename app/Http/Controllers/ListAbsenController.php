3b9a96cc29080a6c02e661b6313bd1b905983f7b
luthfiaqolba <luthfiqolba15@gmail.com>
2024-08-09 15:04:23 +0700

    update

--- app/Http/Controllers/ListAbsenController.php
+++ app/Http/Controllers/ListAbsenController.php
@@ -17,19 +17,19 @@ class ListAbsenController extends Controller
     {
         // Validasi input
         $request->validate([
-            'name' => 'string|max:255',
+            'name' => 'required|string|max:255',
             'typetime' => 'string|max:255',
             'time' => 'date_format:Y-m-d H:i:s',
             'latitude' => 'numeric|between:-90,90',
             'longitude' => 'numeric|between:-180,180',
             'kantorid' => 'string|max:255'
         ]);
-
+        $time = date('Y-m-d H:i:s');
         // Simpan data
         $listAbsen = new ListAbsen();
         $listAbsen->name = $request->input('name');
         $listAbsen->typetime = $request->input('typetime');
-        $listAbsen->time = $request->input('time');
+        $listAbsen->time = $time;
         $listAbsen->latitude = $request->input('latitude');
         $listAbsen->longitude = $request->input('longitude');
         $listAbsen->kantorid = $request->input('kantorid');
