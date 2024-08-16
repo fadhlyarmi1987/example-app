77006446cb366d537e64ac0179ddae95e4af6ec1
fadhly <fadhly.syahputra@gmail.com>
2024-08-16 13:58:52 +0700

    Final 16 Agustus

--- resources/views/auth/tugas.blade.php
+++ resources/views/auth/tugas.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - CV NATUSI</title>
-    <link rel="stylesheet" href="{{ asset('css/datakaryawan.css') }}">
+    <link rel="stylesheet" href="{{ asset('CSS/datakaryawan1.css') }}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
