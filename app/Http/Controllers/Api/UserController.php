2aa77878b91503c165eff72b2d7c4240f7733ac6
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2024-08-16 20:41:32 +0700

    style: typo ListAbsen

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ListAbsenController;
-use App\Models\Listabsen;
+use App\Models\ListAbsen;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
